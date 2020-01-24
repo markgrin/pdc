bae449f58eb71775bbfd71369074a72d778cb051
Lev Melnikov <lew.ka@hotmail.com>
2020-01-24 16:05:36 +0300

    Добавление рассчета по интенсивности

--- src/simulate.cpp
+++ src/simulate.cpp
@@ -12,8 +12,25 @@ struct CallPoint {
     double timestamp;
 };
 
+struct LastCallTime {
+    double time_passed = 0.f;
+    double overtime = 0.f;
+};
+
+std::size_t toNewCalls (const MethodResult& result, LastCallTime& call_time) {
+    if (result.is_rate) {
+        double time_passed = call_time.time_passed - call_time.overtime;
+        if (time_passed && result.rate / time_passed > 1) {
+            call_time.overtime = time_passed - static_cast<std::size_t>(result.rate / time_passed);
+            return static_cast<std::size_t>(result.rate / time_passed);
+        }
+        return 0u;
+    }
+    return result.calls;
+}
+
 std::size_t makeNewCalls (Method& method, const std::vector<Call>& calls, std::list<CallPoint>& call_points,
-                          std::size_t& newCallIndex, std::size_t agents, double & since_last_call) {
+                          std::size_t& newCallIndex, std::size_t agents, LastCallTime & since_last_call) {
     std::vector<double> service_calls, setup_calls;
     for (const auto& call_point : call_points) {
         if (call_point.timestamp > call_point.call.setup)
@@ -21,7 +38,7 @@ std::size_t makeNewCalls (Method& method, const std::vector<Call>& calls, std::l
         else
             setup_calls.push_back(call_point.timestamp);
     }
-    auto result = method.calculate(agents, setup_calls, service_calls).calls;
+    auto result = toNewCalls(method.calculate(agents, setup_calls, service_calls), since_last_call);
     std::size_t new_calls = result;
 
     //std::cout << "Make " << new_calls << "\n";
@@ -33,7 +50,7 @@ std::size_t makeNewCalls (Method& method, const std::vector<Call>& calls, std::l
         CallPoint newCallPoint = {newCall, 0};
         //std::cout << newCall.answered << " : " << newCall.setup << " : " << newCall.service << "\n";
         call_points.push_back(newCallPoint);
-        since_last_call = 0; 
+        since_last_call.time_passed = 0; 
     }
 
     //std::cout << "Free " << agents << "\nsetup_calls:";
@@ -54,7 +71,7 @@ SimResult simulate (double step, std::size_t agents, Method& method, const std::
     std::vector<double> service_calls;
     std::vector<double> setup_calls;
     SimResult result = {0, 0, 0, 0, 0, 0};
-    double since_last_call = 0;
+    LastCallTime since_last_call {};
     while (true) {
         std::size_t free_agents = makeNewCalls(method, calls, call_points, newCallIndex, agents, since_last_call);
 
@@ -98,7 +115,7 @@ SimResult simulate (double step, std::size_t agents, Method& method, const std::
             }
             it++;
         }
-        since_last_call += step;
+        since_last_call.time_passed += step;
         result.duration += step;
         result.wait_time += step * free_agents;
 
---
+++ tests/test_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo -e "help\ngenerate_data\nauto_test\n100000\nB\n0.5\nE\n1\nE\n1\n\
+\nsimulate\nprogressive\nauto_test\n10\n0.1\n" | $1
+
